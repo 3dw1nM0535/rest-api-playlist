85cfe31ec50b57c657c179e9b3df86d2eedf25d8
Edwin Moses <workockmoses@gmail.com>
2017-07-28 16:51:50 +0300

    model for user database

---
+++ models/model.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//Create schema for model or collection
+var UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name field is required']
+  },
+  rank: {
+    type: String
+  },
+  available: {
+    type: Boolean,
+    default: false
+  }
+});
