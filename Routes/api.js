3e33984b64dbd6fee51d3a9c378654756db1269b
Edwin Moses <workockmoses@gmail.com>
2017-07-28 17:55:58 +0300

    delete from db

--- Routes/api.js
+++ Routes/api.js
@@ -5,25 +5,27 @@ var User = require('../models/model');
 var router = express.Router();
 
 //Get list from db
-router.get('/users', function (req, res) {
+router.get('/users', function (req, res, next) {
   res.send({type: 'GET'});
 });
 
 //Add ninja to db
-router.post('/users', function (req, res) {
+router.post('/users', function (req, res, next) {
   User.create(req.body).then(function (data) {
     res.send(data);
-  });
+  }).catch(next);
 });
 
 //Update data in db
-router.put('/users/:id', function (req, res) {
+router.put('/users/:id', function (req, res, next) {
   res.send({type: 'PUT'});
 });
 
 //Delete data in db
-router.delete('/users/:id', function (req, res) {
-  res.send({type: 'DELETE'});
+router.delete('/users/:id', function (req, res, next) {
+  User.findByIdAndRemove({_id: req.params.id }).then(function (user) {
+    res.send(user);
+  });
 });
 
 module.exports = router;
