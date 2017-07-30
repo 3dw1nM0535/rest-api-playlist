763a44054477587b007bcf6ac6b81983c0643c63
Edwin Moses <workockmoses@gmail.com>
2017-07-30 09:42:49 +0300

    Documented code concept

--- Routes/api.js
+++ Routes/api.js
@@ -4,7 +4,7 @@ var User = require('../models/model');
 
 var router = express.Router();
 
-//Get list from db
+//Get user from the db
 router.get('/users', function (req, res, next) {
   User.geoNear(
     { type: "Point", coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
@@ -14,14 +14,14 @@ router.get('/users', function (req, res, next) {
   });
 });
 
-//Add ninja to db
+//Add user to the db
 router.post('/users', function (req, res, next) {
   User.create(req.body).then(function (data) {
     res.send(data);
   }).catch(next);
 });
 
-//Update data in db
+//Update user-data in the db
 router.put('/users/:id', function (req, res, next) {
   User.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
     User.findOne({ _id: req.params.id }).then(function (user) {
@@ -31,11 +31,12 @@ router.put('/users/:id', function (req, res, next) {
   });
 });
 
-//Delete data in db
+//Delete user-data in the db
 router.delete('/users/:id', function (req, res, next) {
   User.findByIdAndRemove({_id: req.params.id }).then(function (user) {
     res.send(user);
   });
 });
 
+//Export module to access it in other modules
 module.exports = router;
